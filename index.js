ca9302d2a4fa6caa40a8d2c80e79ea915933612e
ArtDor1978 <artdor1978@gmail.com>
2022-06-13 11:42:46 +0300

    page103

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 //const BlogPost = require("./models/BlogPost.js");
 const fileUpload = require("express-fileupload");
-const newPostController = require('./controllers/newPost')
-const homeController = require('./controllers/home')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
+const newPostController = require("./controllers/newPost");
+const homeController = require("./controllers/home");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
 const validateMiddleware = require("./middleware/validationMiddleware");
-const newUserController = require('./controllers/newUser')
-const storeUserController = require('./controllers/storeUser')
-const loginController = require('./controllers/login')
-const loginUserController = require('./controllers/loginUser')
-const expressSession = require('express-session');
+const newUserController = require("./controllers/newUser");
+const storeUserController = require("./controllers/storeUser");
+const loginController = require("./controllers/login");
+const loginUserController = require("./controllers/loginUser");
+const expressSession = require("express-session");
+const authMiddleware = require("./middleware/authMiddleware");
+const redirectIfAuthenticatedMiddleware = require("./middleware/redirectIfAuthenticatedMiddleware");
+const logoutController = require("./controllers/logout");
 
 mongoose.connect("mongodb://127.0.0.1:27017/my_database", {
   useNewUrlParser: true,
@@ -32,11 +35,17 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use('/posts/store', validateMiddleware)
-app.use(expressSession({
-  secret: 'keyboard cat'
-}))
-
+app.use("/posts/store", validateMiddleware);
+app.use(
+  expressSession({
+    secret: "keyboard cat",
+  })
+);
+global.loggedIn = null;
+app.use("*", (req, res, next) => {
+  loggedIn = req.session.userId;
+  next();
+});
 /* app.get("/", async (req, res) => {
   const blogposts = await BlogPost.find({});
   res.render("index", {
@@ -58,13 +67,18 @@ app.get("/contact", (req, res) => {
     blogpost,
   });
 }); */
-app.get('/', homeController)
-app.get('/post/:id', getPostController)
-app.post('/posts/store', storePostController)
-app.get('/auth/register', newUserController)
-app.get('/posts/new', newPostController)
-app.get('/auth/login', loginController);
-app.post('/users/login', loginUserController)
+app.get("/", homeController);
+app.get("/post/:id", getPostController);
+app.post("/posts/store", authMiddleware, storePostController);
+app.get("/auth/register", redirectIfAuthenticatedMiddleware, newUserController);
+app.get("/posts/new", authMiddleware, newPostController);
+app.get("/auth/login", redirectIfAuthenticatedMiddleware, loginController);
+app.get("/auth/logout", logoutController);
+app.post(
+  "/users/login",
+  redirectIfAuthenticatedMiddleware,
+  loginUserController
+);
 /* app.post("/posts/store", (req, res) => {
   let image = req.files.image;
   image.mv(
@@ -78,7 +92,12 @@ app.post('/users/login', loginUserController)
     }
   );
 }); */
-app.post('/users/register', storeUserController)
+app.post(
+  "/users/register",
+  redirectIfAuthenticatedMiddleware,
+  storeUserController
+);
+app.use((req, res) => res.render("notfound"));
 app.listen(4000, () => {
   console.log("App listening on port 4000");
 });
